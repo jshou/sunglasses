61eb6eb8413fccd7fa4290070f0415aa59c2eadb
Joshua Hou <joshuahou@gmail.com>
2011-07-26 01:02:03 -0700

    renamed response submissions to avoid confusion

--- sunglasses.js
+++ sunglasses.js
@@ -1,14 +1,14 @@
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
-    if (request.sunglasses_opacity != null) {
+    if (request.tab_sunglasses_opacity != null) {
       if(document.getElementById("webpage_sunglasses") == null) {
         var sunglasses = document.createElement("div");
         sunglasses.id = "webpage_sunglasses";
         document.body.appendChild(sunglasses);
       }
 
-      document.getElementById("webpage_sunglasses").style.opacity = request.sunglasses_opacity;
-      sendResponse({status: "sunglasses.js successfully received sunglasses_opacity " + request.sunglasses_opacity});
+      document.getElementById("webpage_sunglasses").style.opacity = request.tab_sunglasses_opacity;
+      sendResponse({status: "sunglasses.js successfully received sunglasses_opacity " + request.tab_sunglasses_opacity});
     } else {
       sendResponse({status: "sunglasses.js did not receive sunglasses_opacity"});
     }
